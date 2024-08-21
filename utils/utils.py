ea06c3c87c4428dd9b850db30f6dd67f6b1b8283
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-21 00:57:19 -0700

    Create utils.py

---
+++ utils/utils.py
@@ -0,0 +1,10 @@
+import logging
+
+def setup_logging():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def accuracy_score_func(preds, labels):
+    correct = (preds == labels).sum().float()
+    total = len(preds)
+    accuracy = correct / total
+    return accuracy.item()
